9cea33979e1c0c6d9e67cbe6297c39a84a4bbcb6
DreadPirate07 <tanayraikhere@gmail.com>
2025-01-10 00:03:46 +0530

    modified: raytracer.cpp - generates a ppm format picture

---
+++ README.md
@@ -0,0 +1,5 @@
+The goal of render() function is to -
+    1. Create a framebuffer, which is a 3d array of (r,g,b) values.
+    2. Assign a colour to each pixel (element of the array)
+    3. Write the output to a .ppm file (Portable Pixmap)
+PPM is straightforward to write with a small header
--- raytracer.cpp
+++ raytracer.cpp
@@ -1,19 +1,37 @@
 #include <cstddef>
 #include <limits>
-#include <cmath>
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include "geometry.h"
 
+// renders a file of 
 void render() {
     const int width = 1024;
     const int height = 768;
     std::vector<Vec3f> framebuffer(width*height);
 
+    // vec3f values are manipulated from (0,0,0) to (1,1,0)
     for (size_t j=0;j<height;j++) {
         for (size_t i=0;i<width;i++) {
-            framebuffer[i+j*width] = Vec3f(j/float(height))
+            framebuffer[i+j*width] = Vec3f(j/float(height),i/float(width),0);
         }
     }
+
+    std::ofstream ofs;
+    ofs.open("./out.ppm");
+    
+    // PPM header - P6 file format
+    ofs << "P6\n" << width << " " << height << "\n255\n";
+    for (size_t i=0;i<height*width;++i) {
+        for (size_t j=0;j<3;j++) {
+            ofs << (char)(255*std::max(0.f,std::min(1.0f,framebuffer[i][j])));
+        }
+    }
+    ofs.close();
+}
+
+int main() {
+    render();
+    return 0;
 }
